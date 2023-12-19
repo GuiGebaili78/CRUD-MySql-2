7c3159a41421c5d7334dfadac8ebd0c545c7e1ac
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2023-12-19 17:44:54 -0300

    tudo funcionando

--- crudmysql/Models/PacienteModel.cs
+++ crudmysql/Models/PacienteModel.cs
@@ -1,9 +1,16 @@
-﻿namespace crudmysql.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.WebEncoders.Testing;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crudmysql.Models
 {
+    
     public class PacienteModel
     {
-
+        
         public int Id { get; set; }
+        
         public string Name { get; set; }
 
         public PacienteModel()
